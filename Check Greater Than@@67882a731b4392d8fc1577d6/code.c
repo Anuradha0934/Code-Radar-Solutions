9d408e8c3a0df409175557db065a4c2bf67223ad
Anuradha0934 <anuradha0934.becse24@chitkara.edu.in>
2025-04-07 17:27:26 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 
 int main() {
-    char name[50];
-    int age;
-    char hobby[50];
+    float radius;
     printf("");
-    scanf("%s", &name);
-    scanf("%d", &age);
-    scanf("%s", &hobby);
-    printf("Name: %s \n", name);
-    printf("Age: %d \n", age);
-    printf("Hobby: %s \n", hobby);
+    scanf("%f", &radius);
+    printf("Area: %.2f", 3.14 * radius * radius);
     return 0;
 }
